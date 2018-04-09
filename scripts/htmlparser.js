8e83ddcee485bc895804005e6e83b2be21752439
travis <tsdubose@gmail.com>
2018-04-09 15:50:02 -0400

    working commit

--- scripts/htmlparser.js
+++ scripts/htmlparser.js
@@ -2,7 +2,6 @@ const parse5 = require('parse5');
 
 hexo.extend.helper.register('parse5', function(string, picture, accentText){
 	var newFrag = parse5.parseFragment(string);
-	//you need to drop both of these into if statements
 	if (picture) {
 		var pictureElement = parse5.parseFragment("<img class='accent-image' src='" + picture + "'/>");
 		newFrag.childNodes.splice(3, 0, pictureElement.childNodes[0]);
